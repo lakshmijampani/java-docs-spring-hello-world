2c3acfd79967f352f2477250b313f5bad871314a
lakshmijampani <112411384+lakshmijampani@users.noreply.github.com>
2022-09-22 16:07:01 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,7 @@ public class DemoApplication {
 	String sayHello() {
 		System.out.println("Hello world logs test===>");
 		System.out.println("welcome to devops");
+		System.out.println("welcome to pr in devops");
 
 		return "Hello  azure cloud World logs !!!!!!!";
 		
