fc25e6433ed4a06ffed570424d4746f387760fec
lakshmijampani <112411384+lakshmijampani@users.noreply.github.com>
2022-09-30 14:06:08 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 		System.out.println("welcome to devops");
 		System.out.println("welcome to pr in devops");
 
-		return "Hello  azure cloud World logs Updated !!!!!!!";
+		return "Hello  azure cloud World logs Updated small change!!!!!!!";
 		
 		
 	}
